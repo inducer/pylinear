bc3e18f211233eb4bf5780aee9307eeae19600f6
Andreas Kloeckner <ak@ixion.net>
2004-06-24 12:04:32 +0000

    [pylinear @ Arch-1:ak@ixion.net--2003-diplomarbeit%pylinear--mainline--1.0--patch-44] Added test case for codiagonalization

---
+++ test/test_codiag.py
@@ -0,0 +1,25 @@
+import sys
+import pylinear.matrices as num
+import pylinear.linear_algebra as la
+import pylinear.matrix_tools as mtools
+
+
+def off_diag_norm_squared(a):
+    result = 0
+    for i,j in a.indices():
+        if i != j:
+            result += abs(a[i,j])**2
+    return result
+
+def off_diag_norms_squared(matrices):
+    return sum([off_diag_norm_squared(mat) for mat in matrices])
+
+size = 20
+tc = num.Float
+
+matrices = [mtools.makeRandomSPDMatrix(size, tc) for i in range(1)]
+
+print off_diag_norms_squared(matrices)
+q, mats = mtools.codiagonalize(matrices)
+print off_diag_norms_squared(mats)
+print matrices
